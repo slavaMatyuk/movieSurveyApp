9800becd2fbabf25567c5373655d798c22b37074
slavaMatyuk <vyacheslav.matyuk@innowise-group.com>
2022-09-21 16:33:03 +0300

    [develop]: fix-validation

--- src/pages/Main/yupSchema.ts
+++ src/pages/Main/yupSchema.ts
@@ -2,5 +2,10 @@ import * as yup from 'yup';
 
 export const surveyFormSchema = yup.object().shape({
   film: yup.string().max(64).required("Please, enter movie's name"),
-  review: yup.string().required('Please, leave the review about the movie'),
+  review: yup
+    .number()
+    .min(1)
+    .max(5)
+    .typeError('Please, enter the number from 1 to 5')
+    .required('Please, leave the review about the movie'),
 });
